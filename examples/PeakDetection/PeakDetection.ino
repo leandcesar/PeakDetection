adc0292f22998d8921cbcfe7b0fbf10079a21cf4
Leandro César <ccleandroc@gmail.com>
2023-03-13 00:18:23 -0300

    docs: update examples

--- examples/PeakDetection/PeakDetection.ino
+++ examples/PeakDetection/PeakDetection.ino
@@ -1,22 +1,21 @@
 #include <PeakDetection.h>
 
-#define pin A0
 PeakDetection peakDetection;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(pin, INPUT);
+  pinMode(A0, INPUT);
   peakDetection.begin(48, 3, 0.6);
 }
 
 void loop() {
-  double data = (double)analogRead(pin)/512-1;
-  peakDetection.add(data);
-  int peak = peakDetection.getPeak();
-  double filtered = peakDetection.getFilt();
-  Serial.print(data);
-  Serial.print(",");
-  Serial.print(peak);
-  Serial.print(",");
-  Serial.println(filtered);
+    double data = (double)analogRead(A0)/512-1;
+    peakDetection.add(data);
+    int peak = peakDetection.getPeak();
+    double filtered = peakDetection.getFilt();
+    Serial.print(data);
+    Serial.print(",");
+    Serial.print(peak);
+    Serial.print(",");
+    Serial.println(filtered);
 }
